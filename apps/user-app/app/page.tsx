f6e3048e33370d7516f2b0555b6207d22c79fe83
shamitbilla <shamitbilla@gmail.com>
2024-08-31 17:13:10 +0530

    Added aditya

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return <>
-    <div className="bg-red-400">
-        Hi from Shamit :)
+    <div className="bg-blue-400">
+        Hi from Shamit & Aditya :)
     </div>
   </>
 }
